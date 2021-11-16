93a6ca8f1ce481f6d42d2530950856a048acfe68
Azizos11 <azizeelkateb6@gmail.com>
2021-11-16 08:45:45 +0200

    react checkpoint

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import './App.css';
+import { Button, Form, FormGroup, Label, Input } 
+from 'reactstrap';
+import { FacebookLoginButton } from 'react-social-login-buttons';
+
+class App extends Component  {
+  render(){
+    return (
+      <>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+      <Form className="login-form">
+        <h1>
+          <span className="fw-bold">Azizos</span>.tn
+        </h1>
+        <h2 className="text-center">Welcome</h2>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email"/>
+          </FormGroup>
+          <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password"/>
+          </FormGroup>  
+           <Button className="btn-lg btn-dark btn-block">
+            Log in
+            </Button>
+            <div className="text-center pt-3">
+              Or continue with your social account
+            </div>
+            <FacebookLoginButton className="mt-3 mb-3" />
+            <div className="text-center">
+              <a href="/sign-up">Sign up</a>
+              <span className="p-2">|</span>
+              <a href="/Forgot-password">Forgot Password</a>
+            </div>
+
+
+      </Form>
+      </>
+    );
+  }
+}
+
+export default App;
